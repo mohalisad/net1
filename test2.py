4c3b6b78c7731e631a5e627ff326ca08b010f143
msadraei <msadraei@icloud.com>
2019-02-26 10:14:19 +0330

    test2.py

--- test2.py
+++ test2.py
@@ -19,11 +19,11 @@ class MyTopo( Topo ):
         s2 = self.addSwitch( 's2' )
 
         # Add links
-        self.addLink( h1, s1, delay='20ms' )
-        self.addLink( h2, s1, delay='20ms' )
-        self.addLink( s1, s2, delay='50ms' )
-        self.addLink( h3, s2, delay='15ms' )
-        self.addLink( h4, s2, delay='1s'   )
+        self.addLink( h1, s1, delay='20ms',max_queue_size=1000)
+        self.addLink( h2, s1, delay='20ms',max_queue_size=1000)
+        self.addLink( s1, s2, delay='50ms',max_queue_size=1000)
+        self.addLink( h3, s2, delay='15ms',max_queue_size=1000)
+        self.addLink( h4, s2, delay='1s'  ,max_queue_size=1000)
 
 
 topos = { 'mytopo': ( lambda: MyTopo())}
