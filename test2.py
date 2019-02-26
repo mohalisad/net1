7e1c8bcbbf6d2060651c7124ea353b7a779f4ab6
msadraei <msadraei@icloud.com>
2019-02-26 10:35:58 +0330

    fix2

--- test2.py
+++ test2.py
@@ -20,6 +20,6 @@ class MyTopo( Topo ):
         # Add links
         self.addLink(h1,switches[0],delay=self.delay,max_queue_size=self.switch_que,bw=self.bandwidth)
         self.addLink(h2,switches[-1],delay=self.delay,max_queue_size=self.switch_que,bw=self.bandwidth)
-        for i in range(1,self.count-1):
+        for i in range(1,self.count):
             self.addLink(switches[i],switches[i+1],delay=self.delay,max_queue_size=self.switch_que,bw=self.bandwidth)
 topos = { 'mytopo': ( lambda: MyTopo())}
