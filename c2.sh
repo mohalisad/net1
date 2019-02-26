1145ff0c4d0aacea991d8fc9d02d60ec8c65a57c
mohalisad <mohammadalisadraei@gmail.com>
2019-02-26 06:56:38 +0330

    hi

--- c2.sh
+++ c2.sh
@@ -2,7 +2,9 @@
 ip netns add h1
 ip netns add h2
 ip netns add h3
-/usr/share/openvswitch/scripts/ovs-ctl start
+
+ovs-vsctl del-br s1
+ovs-vsctl del-br s2
 ovs-vsctl add-br s1
 ovs-vsctl add-br s2
 ip link add l1-1 type veth peer name l1-2
@@ -10,14 +12,17 @@ ip link add l2-1 type veth peer name l2-2
 ip link add l3-1 type veth peer name l3-2
 ip link add l4-1 type veth peer name l4-2
 
-ip link set l1-2 up
-ip link set l2-2 up
-
 ip link set l1-1 netns h1
 ovs-vsctl add-port s1 l1-2
 
 ip link set l2-1 netns h2
 ovs-vsctl add-port s1 l2-2
 
-ip netns exec h1 ip addr add 10.0.0.1/24 dev l1-2
-ip netns exec h2 ip addr add 10.0.0.2/24 dev l2-2
+ip link set l1-2 up
+ip link set l2-2 up
+
+ip netns exec h1 ip addr add 10.0.0.1/24 dev l1-1
+ip netns exec h2 ip addr add 10.0.0.2/24 dev l2-1
+
+ip netns exec h1 ip link set l1-1 up
+ip netns exec h2 ip link set l2-1 up
