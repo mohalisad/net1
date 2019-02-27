b80a89c3af37f5d9b356ffa7eca63e27adff662a
msadraei <msadraei@icloud.com>
2019-02-28 01:28:02 +0330

    change

--- test3.py
+++ test3.py
@@ -18,5 +18,6 @@ class MyTopo( Topo ):
 net = Mininet(MyTopo(),link=TCLink)
 net.start()
 for i in range(1,len(net.hosts)):
-    net.hosts[i].cmd( 'python /home/mininet/develoer/net1/servant.py &')
+    #net.hosts[i].cmd( 'python /home/mininet/develoer/net1/servant.py &')
+    pass
 CLI(net)
